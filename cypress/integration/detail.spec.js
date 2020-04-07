3895f0e79bfa16849f9d30a45008e89882e75a38
Gabriel Romay Machado <romay.gabriel@gmail.com>
2020-04-07 16:53:07 +0900

    Remove cypress example tests; add some more custom tests in list page, add not found page test

--- cypress/integration/detail.spec.js
+++ cypress/integration/detail.spec.js
@@ -1,4 +1,4 @@
-describe('On Detail Page', function() {
+describe('On Detail Page, navigate', function() {
   it('User checks that list item is rendered', function() {
     cy.visit('http://localhost:3000/#/detail/1');
     cy.contains('room 1');
--- cypress/integration/list.spec.js
+++ cypress/integration/list.spec.js
@@ -1,14 +1,22 @@
-describe('On List Page', function() {
+describe('On List Page, navigate', function() {
   it('User checks that list items are rendered', function() {
     cy.visit('http://localhost:3000/#/list');
     cy.contains('room 1');
     cy.contains('room 2');
-    //expect(true).to.equal(true);
   });
 
   it('User clicks the "room 1" item link', function() {
     cy.visit('http://localhost:3000/#/list');
     cy.contains('room 1').click({ force: true });
     cy.url().should('include', '/detail/1');
+  });
+});
+
+describe('On List Page, interact', function() {
+  it('User writes on search text input', function() {
+    cy.visit('http://localhost:3000/#/list');
+    cy.get('#search')
+      .type('room 3')
+      .should('have.value', 'room 3');
   });
 });
