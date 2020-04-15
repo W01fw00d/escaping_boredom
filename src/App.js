7da1365058186f582c09e0f82fa6f133e989339b
Gabriel Romay Machado <romay.gabriel@gmail.com>
2020-04-15 15:34:21 +0900

    Remove ThemeProvider commented code

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import React from 'react';
 import { hot } from 'react-hot-loader';
 import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
-/* import { ThemeProvider } from '@material-ui/core';
-import theme from './styles/global-styles'; */
-
 import RoomListPage from './pages/roomListPage';
 import RoomDetailPage from './pages/roomDetailPage';
 import NotFoundPage from './pages/notFoundPage';
@@ -13,7 +10,6 @@ const App = () => {
   return (
     <div className="App">
       <Router>
-        {/* <ThemeProvider theme={theme}> */}
         <Switch>
           <Route exact path="/list">
             <RoomListPage />
@@ -24,7 +20,6 @@ const App = () => {
           <Redirect from="/" exact to="/list" />
           <Route component={NotFoundPage} />
         </Switch>
-        {/* </ThemeProvider> */}
       </Router>
     </div>
   );
