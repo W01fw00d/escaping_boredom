9c273e63620584e3e39a15a7d944b065103d2c9a
Gabriel Romay Machado <romay.gabriel@gmail.com>
2020-04-16 18:48:01 +0900

    Convert server.js in a hibrid server for serving bundle.js and listening api calls in production

--- server.js
+++ server.js
@@ -1,14 +1,43 @@
 const express = require('express');
 const path = require('path');
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 4000;
 const app = express();
 
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
+
+const mongoose = require('mongoose'),
+  tagModel = require('./server/api/models/tag-model'),
+  roomModel = require('./server/api/models/room-model'),
+  bodyParser = require('body-parser');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/Tododb', () => {
+  tagModel.initData(tagModel);
+  roomModel.initData(roomModel);
+});
+
+app.use(function (req, res, next) {
+  // We allow all origins for the moment for development purpouses
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const routes = require('./server/api/routes/escaping-boredom-route');
+routes(app);
 
 // send the user to index html page inspite of the url
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
+app.use(({ originalUrl }, res) => res.status(404).send(`"${originalUrl}" endpoint was not found`));
+
 app.listen(port);
+
+console.log('React App started on: ' + port);
+console.log('RESTful API server started on: ' + port);
